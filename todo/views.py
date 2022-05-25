c8426c169345f14818b54f272060d9a77c3016ac
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-25 21:41:48 +0300

    list.html and urls were set

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 
 def home(request):
     return render(request, "todo/home.html")
+
+
+
+def todo_list(request):
+    todos= Todo.objects.all()
+
+    context={
+        "todos":todos
+    }
+
+    return render(request,"todo/todo_list.html",context)
+    
+
+def todo_create(request):
+    pass
+
+def todo_update(request,id):
+    pass
+
+def todo_delete(request,id):
+    pass
