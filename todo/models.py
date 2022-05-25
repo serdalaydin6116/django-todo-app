62c2c81042a627db16b45ec120f66a5a3a7cb03e
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-25 20:49:48 +0300

    model.py was set

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.forms import DateTimeField
 
 # Create your models here.
+
+class Todo(models.Model):
+    title=models.CharField(max_length=50)
+    decription=models.TextField()
+
+    PRIORITY=(
+        ("1", "High"),
+        ("2", "Medium"),
+        ("3", "Low"),
+    )
+    priority=models.CharField(max_length=10, choices=PRIORITY)
+    isCompleted = models.BooleanField(default=False)
+    update_date= models.DateTimeField(auto_now=True)
+    created_date= models.DateTimeField(auto_now_add=True)
